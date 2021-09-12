512aa9ca03bcfdb51027c106d7edb4b9531b5289
bacn <mbachmann@trdi.io>
2021-09-12 19:22:06 +0200

    add headless test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Tooltip} from "bootstrap";
-import {activateTooltip} from "./shared/utils";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Tooltip } from 'bootstrap';
+import { activateTooltip } from './shared/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'contact-book-angular';
 
-  ngOnInit(): void {
-    activateTooltip();
-  }
+  ngOnInit(): void {}
 
-  ngOnDestroy(){
-  }
-
+  ngOnDestroy() {}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_BASE_PATH: 'https://contact-book-h2.united-portal.com'
+  API_BASE_PATH: 'https://contact-book-h2.united-portal.com',
 };
