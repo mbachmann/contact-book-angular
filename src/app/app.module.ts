58f52f051eb1b47c9fe41fb2f62c6aa0e9402922
bacn <mbachmann@trdi.io>
2021-09-16 10:28:09 +0200

    add Toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { AvatarModule } from 'ngx-avatar';
 import { ImageCropperComponent } from './components/image-cropper/image-cropper.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {CommonModule} from "@angular/common";
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 const avatarColors = ['#FFB6C1', '#2c3e50', '#95a5a6', '#cba973', '#1abc9c'];
 
 @NgModule({
@@ -22,12 +23,18 @@ const avatarColors = ['#FFB6C1', '#2c3e50', '#95a5a6', '#cba973', '#1abc9c'];
     HammerModule,
     BrowserModule,
     CommonModule,
+    BrowserAnimationsModule,
     ImageCropperModule,
     AppRoutingModule,
     HttpClientModule,
     ApiModule,
     AvatarModule.forRoot({
       colors: avatarColors,
+    }),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
     }),
     FormsModule,
     ReactiveFormsModule,
