8e17435b683c28e14a99ddf015277628b2d7f4ba
bacn <mbachmann@trdi.io>
2021-09-19 21:46:49 +0200

    add the router feature onSameUrlNavigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,10 +21,11 @@ const routes: Routes = [
     pathMatch: 'full',
     redirectTo: '/home',
   },
+  { path: 'configuration', loadChildren: () => import('./feature-modules/configuration/configuration.module').then(m => m.ConfigurationModule) },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
